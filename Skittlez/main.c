bdced88a0b8f88eeab293d282a4f3889e98dab28
sean.eigle143 <seigle15@gmail.com>
2019-11-26 11:49:05 -0800

    computing averages

--- Skittlez/main.c
+++ Skittlez/main.c
@@ -5,13 +5,22 @@ int main() {
 
     srand(time(0));
     int bagNum = 0;
+    int sum = 0;
 
-    bagNum++;
-    head = addToList(createBag(), head);
-    while(checkForCopy(head, bagNum) != 0){
+
+    for(int i = 0; i < 1000; i++){
+        bagNum = 0;
         head = addToList(createBag(), head);
         bagNum++;
+        while(checkForCopy(head, bagNum) != 0){
+            head = addToList(createBag(), head);
+            bagNum++;
+        }
+        sum += bagNum;
     }
+    int averages;
+
+
 
     printf("Number of bags: %d\n", bagNum);
     printf("The Copy: green %d, orange %d, purple %d, red %d, yellow %d \n", head->data->green, head->data->orange, head->data->purple, head->data->red, head->data->yellow);
